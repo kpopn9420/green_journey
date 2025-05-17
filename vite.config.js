3d8eccb3600b79cc75651408ce1e3a8d7e7c85ef
ET's planet <kpopn9420@gmail.com>
2025-05-18 01:08:54 +0800

    chore: deploy with docs folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/green_journey/',
-  build: { outDir: 'docs' },
   plugins: [react()],
+  base: '/green_journey/', 
 })
